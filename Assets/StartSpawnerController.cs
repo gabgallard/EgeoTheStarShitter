1cb9d0cca5882d61e6402c7e76bb758483377f32
Fernando Guillen <fguillen.mail@gmail.com>
2021-07-07 09:54:01 +0200

    Egeo starts shitting stars

--- Assets/StartSpawnerController.cs
+++ Assets/StartSpawnerController.cs
@@ -11,6 +11,7 @@ public class StartSpawnerController : MonoBehaviour
   [SerializeField] Collider2D skyCollider;
   [SerializeField] GameObject starPrefab;
   [SerializeField] Transform target;
+  [SerializeField] float force;
 
   void Awake()
   {
@@ -21,13 +22,38 @@ public class StartSpawnerController : MonoBehaviour
   [ContextMenu("SpawnStar")]
   public void SpawnStar()
   {
+    StartCoroutine("SpawnStarCoroutine");
+  }
+
+  void Update()
+  {
+      if (Input.GetKeyDown("space"))
+      {
+          SpawnStar();
+      }
+  }
+
+  IEnumerator SpawnStarCoroutine()
+  {
     skyCollider.enabled = true;
     Vector3 position = randomPointInCollider.RandomPoint();
     skyCollider.enabled = false;
     GameObject star = Instantiate(starPrefab, position, Quaternion.identity);
     Transform body = star.transform.Find("Body");
-    body.position = target.position;
-    body.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0));
+    body.position = transform.position;
+    SpringJoint2D bodySpringJoint = body.GetComponent<SpringJoint2D>();
+    bodySpringJoint.enabled = false;
+    Rigidbody2D bodyRigidbody = body.gameObject.GetComponent<Rigidbody2D>();
+    bodyRigidbody.AddForce((target.position - body.position) * force, ForceMode2D.Impulse);
+
+    yield return new WaitForSeconds(3.0f);
+
+    float previousDrag = bodyRigidbody.drag;
+    bodyRigidbody.drag = 10;
+    bodySpringJoint.enabled = true;
+
+    yield return new WaitForSeconds(1.0f);
+    bodyRigidbody.drag = previousDrag;
   }
 
 }

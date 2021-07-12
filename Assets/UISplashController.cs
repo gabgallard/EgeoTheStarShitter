89779ba6a03d2a5a274ef8b8262a4a2a28ef71aa
Fernando Guillen <fguillen.mail@gmail.com>
2021-07-12 18:06:25 +0200

    Splash scene

---
+++ Assets/UISplashController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class UISplashController : MonoBehaviour, IPointerClickHandler
+{
+  Animator animator;
+
+  void Awake()
+  {
+    animator = GetComponent<Animator>();
+  }
+
+  void Start()
+  {
+    Invoke("ShowClickToStart", 3.0f);
+  }
+
+  public void OnPointerClick(PointerEventData eventData)
+  {
+    SceneManager.LoadScene("Game");
+  }
+
+
+  void ShowClickToStart()
+  {
+    animator.SetTrigger("ClickToStart");
+  }
+}

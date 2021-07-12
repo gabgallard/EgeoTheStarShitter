f49cb64d120edb66b5f63d8119fd0d231faedb42
Fernando Guillen <fguillen.mail@gmail.com>
2021-07-12 17:36:05 +0200

    Intro scene with Animator

--- Assets/Scripts/EgeoController.cs
+++ Assets/Scripts/EgeoController.cs
@@ -7,7 +7,6 @@ public class EgeoController : MonoBehaviour
 {
   public static EgeoController Instance;
 
-  [SerializeField] GameObject egeoBody;
   [SerializeField] public Transform MouthInside;
   [SerializeField] MouthController mouthController;
   [SerializeField] MouthController bottomController;
@@ -23,11 +22,6 @@ public class EgeoController : MonoBehaviour
   void Awake()
   {
     Instance = this;
-  }
-
-  public void ShowEgeo()
-  {
-    egeoBody.SetActive(true);
   }
 
   void ShitStar()
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -7,99 +7,22 @@ using DG.Tweening;
 
 public class UIController : MonoBehaviour
 {
-  [SerializeField] TMP_Text text_01;
-  [SerializeField] TMP_Text text_02;
-  [SerializeField] TMP_Text text_03;
-  [SerializeField] Image background;
+  Animator animator;
 
   void Awake()
   {
-    text_01.enabled = false;
-    text_02.enabled = false;
-    text_03.enabled = false;
+    animator = GetComponent<Animator>();
   }
 
   // Start is called before the first frame update
   void Start()
   {
-    StartCoroutine("TitlesCoroutine");
+    animator.SetTrigger("Intro");
   }
 
-  IEnumerator TitlesCoroutine()
+  void StartSky()
   {
-    yield return new WaitForSeconds(1.0f);
-
-    // At the beginning was Egeo
-    text_01.DOFade(0.0f, 0.0f);
-    background.DOFade(0.0f, 0.0f);
-
-    text_01.enabled = true;
-    background.enabled = true;
-
-    background.DOFade(1.0f, 1.0f);
-
-    yield return new WaitForSeconds(2.0f);
-    text_01.DOFade(1.0f, 1.0f);
-
-    EgeoController.Instance.ShowEgeo();
-
-    yield return new WaitForSeconds(3.0f);
-    text_01.DOFade(0.0f, 1.0f);
-
-    yield return new WaitForSeconds(0.5f);
-    background.DOFade(0.0f, 1.0f);
-
-    yield return new WaitForSeconds(1f);
-    text_01.enabled = false;
-    background.enabled = false;
-
-    yield return new WaitForSeconds(6.0f);
-    // At the beginning were the Planets
-    text_02.DOFade(0.0f, 0.0f);
-    background.DOFade(0.0f, 0.0f);
-
-    text_02.enabled = true;
-    background.enabled = true;
-
-    background.DOFade(1.0f, 1.0f);
-
-    yield return new WaitForSeconds(1.5f);
-    text_02.DOFade(1.0f, 1.0f);
-
-    yield return new WaitForSeconds(3.0f);
-    text_02.DOFade(0.0f, 1.0f);
-
-    yield return new WaitForSeconds(1.5f);
-    background.DOFade(0.0f, 1.0f);
-
+    Debug.Log("StartSky()");
     PlanetSpawnerController.Instance.FirstSpawn();
-
-    yield return new WaitForSeconds(1f);
-    text_02.enabled = false;
-    background.enabled = false;
-
-    yield return new WaitForSeconds(6.0f);
-    // but, how the stars appeared?
-    text_03.DOFade(0.0f, 0.0f);
-    background.DOFade(0.0f, 0.0f);
-
-    text_03.enabled = true;
-    background.enabled = true;
-
-    background.DOFade(1.0f, 1.0f);
-
-    yield return new WaitForSeconds(1.5f);
-    text_03.DOFade(1.0f, 1.0f);
-
-    yield return new WaitForSeconds(3.0f);
-    text_03.DOFade(0.0f, 1.0f);
-
-    yield return new WaitForSeconds(1.5f);
-    background.DOFade(0.0f, 1.0f);
-
-    yield return new WaitForSeconds(1f);
-    text_03.enabled = false;
-    background.enabled = false;
   }
-
 }

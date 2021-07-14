502d357a88e3586882b70f6cb521a0bf7b69f7e8
Fernando Guillen <fguillen.mail@gmail.com>
2021-07-14 15:32:53 +0200

    Sky white flash when wonderful universe finished

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIController : MonoBehaviour
 {
   public static UIController Instance;
   Animator animator;
 
+
   [SerializeField] GameObject infoPage;
+  Camera theCamera;
 
   void Awake()
   {
     animator = GetComponent<Animator>();
     Instance = this;
+    theCamera = Camera.main;
   }
 
   // Start is called before the first frame update
@@ -36,8 +40,18 @@ public class UIController : MonoBehaviour
     infoPage.transform.Find("Scroll").GetComponent<ScrollRect>().verticalNormalizedPosition = 1;
   }
 
+  [ContextMenu("XXX")]
   public void ShowWonderfulUniverseMessage()
   {
     animator.SetTrigger("Wonderful");
+  }
+
+  void SkyWhiteFlash()
+  {
+    Sequence sequence = DOTween.Sequence();
+    sequence.Append(theCamera.DOColor(new Color(0.9764706f, 0.9529412f, 0.9686275f), 0.10f));
+    sequence.Append(theCamera.DOColor(new Color(0f, 0f, 0f), 0.10f));
+    sequence.Append(theCamera.DOColor(new Color(0.9764706f, 0.9529412f, 0.9686275f), 0.20f));
+    sequence.Append(theCamera.DOColor(new Color(0f, 0f, 0f), 0.50f));
   }
 }
